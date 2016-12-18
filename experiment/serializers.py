2284c70dc39698e370c89f922251f815b9aabd5c
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-12-18 16:47:00 +0100

    Enable embeding settings into experiment

--- experiment/serializers.py
+++ experiment/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from experiment.models import Experiment
 from experiment_session.serializers import ExperimentSessionSerializer
+from experiment_preset.serializers import ExperimentPresetSerializer
 
 
 class ExperimentSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = Experiment
-        fields = ('id', 'name', 'settings', 'createdon', 'status', 'sessions')
-        read_only = ('createdon', 'sessions',)
+        fields = ('id', 'name', 'settings', 'createdon', 'status', 'sessions',)
+        read_only = ('createdon', 'sessions', 'status',)
         include = {
-            'sessions': ExperimentSessionSerializer()
+            'sessions': ExperimentSessionSerializer(),
+            'settings': ExperimentPresetSerializer()
         }
