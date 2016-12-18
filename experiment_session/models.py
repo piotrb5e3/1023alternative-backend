9d03bfe98ef2933fa9549882ef49b83f35210b7d
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-12-18 11:42:44 +0100

    Session number and progress; combination status

--- experiment_session/models.py
+++ experiment_session/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
-# Create your models here.
 class ExperimentSession(models.Model):
+    class Meta:
+        unique_together = ('experiment', 'number',)
+
     STATUS_FINISHED = 'F'
     STATUS_IN_PROGRESS = 'P'
     _STATUS_CHOICES = (
@@ -13,10 +15,23 @@ class ExperimentSession(models.Model):
 
     experiment = models.ForeignKey('experiment.Experiment', related_name='sessions')
     status = models.CharField(max_length=1, default=STATUS_IN_PROGRESS, choices=_STATUS_CHOICES)
+    number = models.IntegerField(validators=(MinValueValidator(1),))
+
+    @property
+    def progress(self):
+        return self.combinations.filter(status=Combination.STATUS_FINISHED).count() / 1023
 
 
 class Combination(models.Model):
+    STATUS_FINISHED = 'F'
+    STATUS_IN_PROGRESS = 'P'
+    _STATUS_CHOICES = (
+        (STATUS_FINISHED, "Finished"),
+        (STATUS_IN_PROGRESS, "In progress"),
+    )
+
     session = models.ForeignKey(ExperimentSession, related_name='combinations')
+    status = models.CharField(max_length=1, default=STATUS_IN_PROGRESS, choices=_STATUS_CHOICES)
     lightset = models.IntegerField(
         validators=(
             MinValueValidator(1),
--- experiment_session/serializers.py
+++ experiment_session/serializers.py
@@ -7,7 +7,7 @@ from experiment_event.serializers import EventSerializer
 class CombinationSerializer(ModelSerializer):
     class Meta:
         model = Combination
-        fields = ('id', 'session', 'lightset', 'events',)
+        fields = ('id', 'session', 'lightset', 'events', 'status')
         include = {
             'events': EventSerializer()
         }
@@ -16,7 +16,8 @@ class CombinationSerializer(ModelSerializer):
 class ExperimentSessionSerializer(ModelSerializer):
     class Meta:
         model = ExperimentSession
-        fields = ('id', 'experiment', 'status', 'combinations',)
+        fields = ('id', 'experiment', 'status', 'combinations', 'number', 'progress')
+        read_only_fields = ('id', 'progress',)
         include = {
             'combinations': CombinationSerializer()
         }
