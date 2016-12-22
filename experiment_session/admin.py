1343a8844a8ea2c4bfc02a14735a06af581f8841
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-12-22 19:00:09 +0100

    Add Repeats model to admin

--- experiment_session/admin.py
+++ experiment_session/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from experiment_session.models import ExperimentSession, Combination
+from experiment_session.models import ExperimentSession, Combination, Repeat
 
 # Register your models here.
 admin.site.register(ExperimentSession)
 admin.site.register(Combination)
+admin.site.register(Repeat)
