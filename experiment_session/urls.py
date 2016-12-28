6e512d4a40636c198d67abe4b529f4665c20b469
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-12-28 15:54:36 +0100

    Add endpoint for retrieving experiment settings

--- experiment_session/urls.py
+++ experiment_session/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
-from experiment_session.views import ExperimentSessionViewSet
+from experiment_session.views import ExperimentSessionViewSet, get_experiment_settings
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'experiment-sessions', ExperimentSessionViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^extra/get-experiment', get_experiment_settings),
 ]
--- experiment_session/views.py
+++ experiment_session/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets, permissions
-from experiment_session.models import ExperimentSession
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from experiment_session.models import ExperimentSession, STATUS_FINISHED
 from experiment_session.serializers import ExperimentSessionSerializer
 
 
@@ -7,3 +9,27 @@ class ExperimentSessionViewSet(viewsets.ModelViewSet):
     queryset = ExperimentSession.objects.all()
     serializer_class = ExperimentSessionSerializer
     permission_classes = (permissions.AllowAny,)
+
+
+@api_view()
+def get_experiment_settings(request):
+    if not ('userid' in request.GET and 'userpass' in request.GET):
+        return Response('No credentials supplied', status=status.HTTP_401_UNAUTHORIZED)
+
+    userid = request.GET['userid']
+    userpass = request.GET['userpass']
+    session = ExperimentSession.objects.filter(userid=userid, userpass=userpass).first()
+    if not session:
+        return Response('No session found', status=status.HTTP_401_UNAUTHORIZED)
+
+    if session.status == STATUS_FINISHED:
+        return Response('Session finished', status=status.HTTP_410_GONE)
+
+    return Response({
+        'sessionId': session.id,
+        'audiomode': session.experiment.audiomode,
+        'lightoffmode': session.experiment.lightoffmode,
+        'lightofftimeout': session.experiment.lightofftimeout,
+        'askUserData': False if session.userage else True,
+        'runTrainingSession': session.showtraining,
+    })
